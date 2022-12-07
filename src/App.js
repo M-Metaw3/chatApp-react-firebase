41a4a88271dd24044b0c6d65190926e8bf8b56ef
M-Metaw3 <mizoomizoo161@gmail.com>
2022-12-07 08:26:18 +0200

    FIREBASE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Rigistet from './pages/Rigistet';
+import { fire } from './Firebase';
+import { useEffect } from 'react'
+
 function App() {
+
   return (
     <div className="App">
 <Rigistet/>
